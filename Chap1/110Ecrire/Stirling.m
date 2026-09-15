8a84a3b6039714b54984bf642c72ab1e476ca356
Prof Ray <58089503+rpanneton@users.noreply.github.com>
2026-09-15 13:13:04 -0400

    Update and rename Sterling.m to Stirling.m

---
+++ Chap1/110Ecrire/Stirling.m
@@ -0,0 +1,9 @@
+% Stirling.m
+
+% ******* RENDRE CE SCRIPT PLUS LISIBLE - VOIR SOLUTIONS PRÉCÉDENTES *****
+
+clear ; clc
+N = input('Écrire la valeur de N : ');
+Factorielle = sqrt(2*pi*N)...
+   /exp(N)...
+   *N^N
