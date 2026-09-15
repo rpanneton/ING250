c1946109e91ad494819d8862b091f7b4756cb94f
Prof Ray <58089503+rpanneton@users.noreply.github.com>
2026-09-15 12:57:17 -0400

    Modify comment for Iris.xls file reference
    Updated comment to include a hyperlink for the Iris.xls file.

--- Chap4/Scripts/Iris.m
+++ Chap4/Scripts/Iris.m
@@ -3,72 +3,72 @@
 % <<..\IrisSepalePetale.gif>>
 % 
 % Objectif : utiliser les indices matriciels pour
-% manipuler une matrice appelée *C*.
+% manipuler une matrice appelÃ©e *C*.
 %
 % Utiliser la matrice *C*
 % dans tous les calculs et fonctions.
 % Les noms de variables en gras sont obligatoires,
 % et sauf avis contraire,
 %
-% * afficher les résultats avec fprintf,
-% * choisir la boucle la plus appropriée (for,
+% * afficher les rÃ©sultats avec fprintf,
+% * choisir la boucle la plus appropriÃ©e (for,
 % while et vectorisation).
 
-%% Étape 1 – lecture
+%% Ã‰tape 1 â€“ lecture
 % Commencer par examiner le contenu du fichier
 % Iris.xls.
 %
-% Lire le fichier Iris.xls et attribuer son
-% contenu à la matrice *C*.
+% Lire le fichier Iris.xls <a href="Iris.xls">Iris.xls</a> et attribuer son
+% contenu Ã  la matrice *C*.
 %
 
-%% Étape 2 – surface d'un sépale et d'un pétale
-% Pour chaque iris, tracer la surface d'un pétale
-% en fonction de celle d'un sépale.
+%% Ã‰tape 2 â€“ surface d'un sÃ©pale et d'un pÃ©tale
+% Pour chaque iris, tracer la surface d'un pÃ©tale
+% en fonction de celle d'un sÃ©pale.
 %
 % Pour simplifier les calculs, poser que la
-% surface d'un sépale ou d'un pétale est égale au
+% surface d'un sÃ©pale ou d'un pÃ©tale est Ã©gale au
 % produit de la longueur par la largeur.
 %
 % Reproduire la figure ci-dessous. Faire en sorte
-% que la légende ne recouvre pas les données.
+% que la lÃ©gende ne recouvre pas les donnÃ©es.
 %
 %  >> doc legend
 % 
 % <<..\IrisSurface.png>>
 
-%% Étape 3 – dimensions du plus gros pétale
-% Déterminer les dimensions (longueur et largeur)
-% du pétale ayant la plus grande surface.
+%% Ã‰tape 3 â€“ dimensions du plus gros pÃ©tale
+% DÃ©terminer les dimensions (longueur et largeur)
+% du pÃ©tale ayant la plus grande surface.
 %
-% * Examiner la figure pour déterminer s'il
+% * Examiner la figure pour dÃ©terminer s'il
 % s'agit d'un iris Virginica ou d'un iris
 % Versicolor.
 %
 % * Afficher la valeur de la surface ainsi que sa
 % largeur et sa longueur avec fprintf.
 %
-% * Ajouter une étoile verte sur la donnée du
-% graphique correspondant à cet iris.
+% * Ajouter une Ã©toile verte sur la donnÃ©e du
+% graphique correspondant Ã  cet iris.
 %
 %        plot(    ...      ,'*g')
 
-%% Étape 4 – critère de séparation
-% Examiner la figure pour déterminer un
-% critère permettant de distinguer qu'un iris est
+%% Ã‰tape 4 â€“ critÃ¨re de sÃ©paration
+% Examiner la figure pour dÃ©terminer un
+% critÃ¨re permettant de distinguer qu'un iris est
 % de la famille Virginica ou de la famille
-% Versicolor à partir de ses dimensions. Le
-% critère doit être le plus efficace possible. Il
-% se peut qu'il ne puisse être
-% discriminant à 100% pour tous les iris.
+% Versicolor Ã  partir de ses dimensions. Le
+% critÃ¨re doit Ãªtre le plus efficace possible. Il
+% se peut qu'il ne puisse Ãªtre
+% discriminant Ã  100% pour tous les iris.
 %
-% Écrire 5 à 6 lignes *en commentaire* pour
-% expliquer le critère. L'explication doit
+% Ã‰crire 5 Ã  6 lignes *en commentaire* pour
+% expliquer le critÃ¨re. L'explication doit
 % s'afficher correctement en texte html contigu
 % avec l'utilitaire _Publish to html_.
 %
-% Rédiger ensuite quelques lignes de code pour
-% démontrer l'efficacité du critère.
+% RÃ©diger ensuite quelques lignes de code pour
+% dÃ©montrer l'efficacitÃ© du critÃ¨re.
 %
 % <IrisSol.html Solution>
 %
